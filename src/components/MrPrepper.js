863004d963a60478ca55c5f9fc0cb10701ed9052
Piotr S <84278062+PiotrSyjud@users.noreply.github.com>
2022-06-08 23:31:11 +0200

    router

--- src/components/MrPrepper.js
+++ src/components/MrPrepper.js
@@ -1,13 +1,26 @@
 import React from "react"
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Backpack from "./Backpack";
+import NavBar from "./Navbar"
+import HomePage from "./HomePage";
+
 
 const MrPrepper = () => {
 
     return (
-        <>
-            <h1> Mr.Prepper </h1>
-            <Backpack/>
-        </>
+        <Router>
+            <div>
+                <h1> Mr.Prepper </h1>
+                <NavBar/>
+                <div>
+                    <Routes>
+                        <Route path="/" component={HomePage}/>
+                        <Route path="/instructions" component={Backpack}/>
+                        <Route path="/shop"/>
+                    </Routes>
+                </div>
+            </div>
+        </Router>
     )
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () => {
+    return (
+        <>
+            <a href='/'>Home</a>
+            <a href="/instructions">instructions</a>
+            <a href="/shop">Shop</a>
+        </>
+    )
+}
+
+export default NavBar
