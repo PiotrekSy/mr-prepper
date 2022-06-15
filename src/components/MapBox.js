6c2a63d100e862e42e8bac4de35b824c7b10483a
Piotr S <84278062+PiotrSyjud@users.noreply.github.com>
2022-06-15 19:25:58 +0200

    navbar dolny w mapie

--- src/components/MapBox.js
+++ src/components/MapBox.js
@@ -1,13 +1,19 @@
 import React from "react";
 
 import NavBar from "./Navbar";
+import MapNavBar from "./MapNavBar"
 
 
 const MapBox = () => {
-    return <>
+    return <div className="mapBox">
         <NavBar/>
-        <h1> MAPBOX</h1>
-    </>
+        {/*mapbox docelowy*/}
+        <div className = "mapBox">
+asdas
+
+        </div>
+        <MapNavBar />
+    </div>
 }
 
 export default MapBox
---
+++ src/components/MapNavBar.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+
+const MapNavBar = () => {
+
+    return (
+        <div className="mapNavBar">
+            <div className="wholeButton">
+                <button className="mapNavButtonBack">
+                    <p className="mapNavButton">a</p>
+                </button>
+            </div>
+            <div className="wholeButton">
+                <button className="mapNavButtonBack">
+                    <p className="mapNavButton">b</p>
+                </button>
+            </div>
+            <div className="wholeButton">
+                <button className="mapNavButtonBack">
+                    <p className="mapNavButton">c</p>
+                </button>
+            </div>
+            <div className="wholeButton">
+                <button className="mapNavButtonBack">
+                    <p className="mapNavButton">d</p>
+                </button>
+            </div>
+            <div className="wholeButton">
+                <button className="mapNavButtonBack">
+                    <p className="mapNavButton">e</p>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default MapNavBar
